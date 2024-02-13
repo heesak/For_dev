7de032a9feabfb68fab9139bcc68c1a11a208409
heesak <adesuyiojo04@gmail.com>
2024-02-13 15:06:02 +0100

    done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
  var hh = document.querySelector('.bars') 
+ var mobile_u = document.querySelector('.mobile-ul')
 
  function addclass(){
     hh.classList.toggle("show")
+    mobile_u.classList.toggle("hey")
  }
